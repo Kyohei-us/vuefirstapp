1599a5780a3e5ecb35ea00f8167445bd856fbffc
Kyohei Yamaguchi <kyohei1212000@gmail.com>
2020-09-16 22:30:09 -0700

    dist directory is created

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,103 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <Menu itemName="aitemu" />
+    <div>{{ msg }}</div>
+    <input v-model="message" placeholder="edit me" />
+    <p>Message is: {{ message }}</p>
+    <button v-on:click="getSong">get lyric path</button>
+    <p>This is the result: {{ songAPIresult }}</p>
+    <button v-on:click="getLyric">get lyric</button>
+    <p>This is the lyric: {{ songLyricResult }}</p>
+    <div>to do is: {{ todosList }}</div>
   </div>
 </template>
 
 <script>
+import Menu from "./menu.vue";
+import axios from "axios";
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  methods: {
+    getSong: function() {
+      // console.log(this.message);
+      axios
+        .get(
+          "https://cors-anywhere.herokuapp.com/" +
+            "https://lyricsapiforme.herokuapp.com/lyric_path/?data=" +
+            this.message,
+          {
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response);
+          this.songAPIresult = response.data.data;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+
+    getLyric: function() {
+      // console.log(this.message);
+      axios
+        .get(
+          "https://cors-anywhere.herokuapp.com/" +
+            "https://lyricsapiforme.herokuapp.com/lyric_content/?data=" +
+            this.songAPIresult,
+          {
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response);
+          this.songLyricResult = response.data.data;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+  },
+  components: {
+    Menu,
+  },
+  data() {
+    return {
+      todosList: null,
+      message: "",
+      songAPIresult: "",
+      songLyricResult: "",
+    };
+  },
+  mounted() {
+    axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/" +
+          "https://lyricsapiforme.herokuapp.com/?data=" +
+          "adhd",
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        this.todosList = response.data.data;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
