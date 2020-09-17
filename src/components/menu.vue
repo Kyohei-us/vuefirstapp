01ea8074b9663240f5ed66e58b7ff8536c1cead2
Kyohei Yamaguchi <kyohei1212000@gmail.com>
2020-09-16 22:30:21 -0700

    dist directory is created

---
+++ src/components/menu.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <nav class="menunavbar">
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">{{ itemName }}</a>
+        </li>
+        <li>
+          <a href="/">3 item</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    itemName: String,
+  },
+};
+</script>
+
+<style scoped>
+.menunavbar {
+  border: solid black;
+  padding: 10px;
+  height: 50px;
+}
+
+.menunavbar > ul > li {
+  padding: 20px;
+  /* float: left;*/
+  display: inline;
+}
+</style>
